9a4bd6f2deb98748609e0ef9e6cf9d1bf32ea4b5
doctorseus <doctorseus@devlabs.pro>
2013-10-14 19:22:48 +0200

    Started implementing the MessageManager.java to process new messages.

--- CBoxBackend/src/eu/drseus/cb/backend/forum/chat/Message.java
+++ CBoxBackend/src/eu/drseus/cb/backend/forum/chat/Message.java
@@ -16,6 +16,12 @@ public class Message {
 
 	private ArrayList<String> messageHistory = new ArrayList<>();
 	
+	@SuppressWarnings("unchecked")
+	public Message(Message msg){
+		this(msg.id, msg.user, msg.time, msg.message);
+		messageHistory = (ArrayList<String>) msg.messageHistory.clone();
+	}
+	
 	public Message(long id, User user, Date time, String message) {
 		super();
 		this.id = id;
@@ -24,10 +30,47 @@ public class Message {
 		this.message = message;
 	}
 
+	public boolean checkChange(Message newMessage){
+		if(!this.message.equals(newMessage.message)){
+			//Do not modify the message here!
+			//messageHistory.add(this.message);
+			//this.message = newMessage.message;
+			return true;
+		}
+		return false;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public ArrayList<String> getMessageHistory() {
+		return messageHistory;
+	}
+
 	@Override
 	public String toString() {
 		return "Message [id=" + id + ", user=" + user + ", time=" + time
 				+ ", message=" + message + "]";
+	}
+
+	public static Message updateHistory(Message oldMessage, Message newMessage) {
+		Message resultMessage = new Message(oldMessage);
+		resultMessage.messageHistory.add(oldMessage.message);
+		resultMessage.message = newMessage.message;
+		return resultMessage;
 	}
 
 }
--- CBoxBackend/src/eu/drseus/cb/backend/util/MessageCache.java
+++ CBoxBackend/src/eu/drseus/cb/backend/util/MessageCache.java
@@ -8,8 +8,23 @@ public class MessageCache {
 
 	private LinkedHashMap<Long, Message> hashMap = new LinkedHashMap<>();
 
-	public MessageCache() {
+	public MessageCache(int size) {
 
+	}
+
+	public boolean contains(long messageId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Message get(long messageId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void put(Message m) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
